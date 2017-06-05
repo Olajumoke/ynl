0c62bce9fb2528a25c22eb474556f2d92329db2d
Jumzy <olajumoke.ca@gmail.com>
2017-06-05 17:56:09 -0400

    error msg for log in

--- general/views.py
+++ general/views.py
@@ -60,7 +60,7 @@ def user_login(request):
 		else:
 			# Bad login details were provided. So we can't log the user in.
 			print "Invalid login details: {0}, {1}".format(username, password)
-			return HttpResponse("Invalid login details supplied.")
+			return render(request, 'general/sign_in.html', {'error_msg':"Invalid login details supplied."})
 
     # The request is not a HTTP POST, so display the login form.
     # This scenario would most likely be a HTTP GET.
