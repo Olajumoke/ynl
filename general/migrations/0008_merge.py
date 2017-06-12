b0a32a95dd946030633c084f1f553fb743569cfb
Abiodun Ajibike <a.ajibike@zaposta.com>
2017-06-12 13:48:18 +0100

    fixed merge files

---
+++ general/migrations/0008_merge.py
@@ -0,0 +1,15 @@
+# -*- coding: utf-8 -*-
+from __future__ import unicode_literals
+
+from django.db import migrations, models
+
+
+class Migration(migrations.Migration):
+
+    dependencies = [
+        ('general', '0003_auto_20170610_2311'),
+        ('general', '0007_auto_20170612_1311'),
+    ]
+
+    operations = [
+    ]
