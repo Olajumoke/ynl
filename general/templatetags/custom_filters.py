24d9c8acc6c73301e99c7b60ab81a8f48369b3c3
Jumzy <olajumoke.ca@gmail.com>
2017-07-25 18:20:27 -0400

    changes

--- general/templatetags/custom_filters.py
+++ general/templatetags/custom_filters.py
@@ -50,12 +50,15 @@ def getCategoryCount(value):
 
 @register.simple_tag
 def getPercent(value,pk):
+	print "val",value
 	if value == 0:
 		percent_value = 0
 		return percent_value
 	else:
 		event_total_players = Event.objects.get(pk=pk)
-		percent_value = (float(value) / float(event_total_players.total_players)) * 100
+		print "tot_players",event_total_players.total_players
+		percent_value = (float(value) / float(event_total_players.total_players())) * 100
+		print "%",percent_value
 		return percent_value
 	
 # @register.simple_tag
